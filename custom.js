5b279726d7e30ce591fe191a0b9a8a53d2b7d599
MkLHX <mickael.lehoux@gmail.com>
2016-03-02 00:20:26 +0100

    Added files via upload
    modifications

--- custom.js
+++ custom.js
@@ -1,58 +1,79 @@
 $(function(){
+	// memo position scrollbar
+	$(window).scroll(function(){
+		scrollPosition = $(window).scrollTop();
+	});
 	// navigation
 	$("#content").load("page.html #home");
 	$("#home").click(function(){
 		$(this).addClass('selectli');
 		$("#page1,#page2,#page3,#page4,#page5,#page6,#page7,#page8").removeClass('selectli');
-		$("#content").load("page.html #home");
+		$("#content").load("page.html #home", function(){
+			$(window).scrollTop(scrollPosition);
+		});
 	});
 	$("#page1").click(function(){
 		$(this).addClass('selectli');
 		$("#home,#page2,#page3,#page4,#page5,#page6,#page7,#page8").removeClass('selectli');
-		$("#content").load("page.html #page1content");
+		$("#content").load("page.html #page1content", function(){
+			$(window).scrollTop(scrollPosition);
+		});
 	});
 	$("#page2").click(function(){
 		$(this).addClass('selectli');
 		$("#home,#page1,#page3,#page4,#page5,#page6,#page7,#page8").removeClass('selectli');
-		$("#content").load("page.html #page2content");
+		$("#content").load("page.html #page2content", function(){
+			$(window).scrollTop(scrollPosition);
+		});
 	});
 	$("#page3").click(function(){
 		$(this).addClass('selectli');
 		$("#home,#page1,#page2,#page4,#page5,#page6,#page7,#page8").removeClass('selectli');
-		$("#content").load("page.html #page3content");
+		$("#content").load("page.html #page3content", function(){
+			$(window).scrollTop(scrollPosition);
+		});
 	});
 	$("#page4").click(function(){
 		$(this).addClass('selectli');
 		$("#home,#page1,#page2,#page3,#page5,#page6,#page7,#page8").removeClass('selectli');
-		$("#content").load("page.html #page4content");
+		$("#content").load("page.html #page4content", function(){
+			$(window).scrollTop(scrollPosition);
+		});
 	});
 	$("#page5").click(function(){
 		$(this).addClass('selectli');
 		$("#home,#page1,#page2,#page3,#page4,#page6,#page7,#page8").removeClass('selectli');
-		$("#content").load("page.html #page5content");
+		$("#content").load("page.html #page5content", function(){
+			$(window).scrollTop(scrollPosition);
+		});
 	});
 	$("#page6").click(function(){
 		$(this).addClass('selectli');
 		$("#home,#page1,#page2,#page3,#page4,#page5,#page7,#page8").removeClass('selectli');
-		$("#content").load("page.html #page6content");
+		$("#content").load("page.html #page6content", function(){
+			$(window).scrollTop(scrollPosition);
+		});;
 	});
 	$("#page7").click(function(){
 		$(this).addClass('selectli');
 		$("#home,#page1,#page2,#page3,#page4,#page5,#page6,#page8").removeClass('selectli');
-		$("#content").load("page.html #page7content");
+		$("#content").load("page.html #page7content", function(){
+			$(window).scrollTop(scrollPosition);
+		});
 	});
 	$("#page8").click(function(){
 		$(this).addClass('selectli');
 		$("#home,#page1,#page2,#page3,#page4,#page5,#page6,#page7").removeClass('selectli');
-		$("#content").load("page.html #page8content");
+		$("#content").load("page.html #page8content", function(){
+			$(window).scrollTop(scrollPosition);
+		});
 	});
 	
 	// bp back to top
 	// scroll distance to display bp
-	var amountScrolled = 450;
-	
-	$(window).scroll(function() {
-		if ( $(window).scrollTop() > amountScrolled ) {
+	var amountScrolled = 350;
+	$('#content').scroll(function() {
+		if ( $('#content').scrollTop() > amountScrolled ) {
 			$('a.back-to-top').fadeIn('slow');
 		} else {
 			$('a.back-to-top').fadeOut('slow');
@@ -60,7 +81,7 @@ $(function(){
 	});
 	// bp action to up
 	$('a.back-to-top').click(function() {
-	$('html, body').animate({
+	$('#content').animate({
 		scrollTop: 0
 	}, 700);
 	return false;
